619b6bc46058f87d71b6b75e26f7d06153ac08c7
Carlos Eduardo <carlosggomes2017@gmail.com>
2023-01-14 12:38:38 -0300

    Pratica

---
+++ ex_3_Pratica/script.js
@@ -0,0 +1,17 @@
+//MAP
+
+// executa em uma função em cada item da lista e retorna uma nova lista com o resultado
+const numbers = [1, 2, 3, 4, 5];
+const numbersMultipliedByTwo = numbers.map(function (number) {
+    return number * 2;
+});
+console.log(numbersMultipliedByTwo);
+
+
+const numero =[,4,6,7,8,9]
+const contarpordois = numero.map(function(numero){
+    return numero *2
+
+});
+
+console.log(contarpordois)
