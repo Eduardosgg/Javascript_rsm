54dea2c729cb762df5731fee63abb106079523e9
Carlos Eduardo <carlosggomes2017@gmail.com>
2023-01-17 11:06:02 -0300

    estudos

---
+++ ex_5_object_pratica/script.js
@@ -0,0 +1,21 @@
+const todos = [
+    {
+        id: 1,
+        description: "Estudar programação",
+        isCompleted: false,
+    },
+    {
+        id: 2,
+        description: "Ler",
+        isCompleted: true,
+    },
+    {
+        id: 3,
+        description: "treinar",
+        isCompleted: true,
+    },
+];
+
+const treino = todos[2]
+
+console.log(treino)
